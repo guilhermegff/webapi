a4e005b1941eab52226aece5c0bbeac18c463961
Guilherme Freire <gfreire@MB.local>
2023-01-24 20:47:58 -0300

    Add tests

---
+++ webapi/datasource/tests/test_models.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from rest_framework import status
+from django.test import TestCase
+from ..models import User
+
+
+# Create your tests here.
+class FirstTest(TestCase):
+    def setUp(self):
+        User.objects.create(name='Bob', status='offline')
+
+    def test_user_name(self):
+        user_bob = User.objects.get(name='Bob')
+        self.assert_(user_bob.name, "Name is Bob")
