7c3cabb68f291138e25ea4bee7a9d8703f145abb
Guilherme Freire <gfreire@MB.local>
2023-01-27 14:44:05 -0300

    Update test

--- webapi/datasource/tests/test_models.py
+++ webapi/datasource/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from rest_framework import status
 from django.test import TestCase
-from ..models import User
+from webapi.datasource.models import User
 
 
 # Create your tests here.
